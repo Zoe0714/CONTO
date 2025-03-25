f3dc92c2389bacee9200cd718a0d4f4bda406d7e
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-03-25 10:27:39 -0300

    Add files via upload

---
+++ site_contos.js
@@ -0,0 +1,6 @@
+const lamp = document.getElementById('conto1');
+
+function EntrarSite(){
+       window.location.href='file:///C:/3TEC/COMPUTACAO_GRAFICA/SITE_CONTOS-main/conto1.html';
+}
+conto1.addEventListener('dblclick',EntrarSite);
