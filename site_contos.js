1652d71b13d8dfeef23d70b01df4b1cab6609e16
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-03-28 07:51:00 -0300

    Update site_contos.js

--- site_contos.js
+++ site_contos.js
@@ -1,6 +1,6 @@
 const lamp = document.getElementById('conto1');
 
 function EntrarSite(){
-       window.location.href='https://zoe0714.github.io/CONTO/';
+       window.location.href='https://zoe0714.github.io/CONTO/conto1.html';
 }
 conto1.addEventListener('dblclick',EntrarSite);
